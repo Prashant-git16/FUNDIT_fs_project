99346f43fd29d6798ddf034decc75ff12aa3d101
Prashant-git16 <prashantmars09@gmail.com>
2026-04-23 09:59:46 +0530

    Fix Axios to use Render API link in Vercel

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 import './index.css';
 import App from './App';
+
+// Set global Axios base URL so Vercel knows how to talk to Render
+if (process.env.REACT_APP_API_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
